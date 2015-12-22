a69abbd58f332a20dca7ae24074a4140f6b94c99
Said Babayev <said@indeed.com>
2015-12-22 10:57:28 +0000

    Channel list now works

--- record_manager.py
+++ record_manager.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python
 import requests
 
+from bs4 import BeautifulSoup
 from flask import Flask, Config
 from flask import render_template
 
-app = Flask(__name__)
+class RecordManagerServer(Flask):
+    def __init__(self, *args, **kwargs):
+        super(RecordManagerServer, self).__init__(*args, **kwargs)
+        self._channels = {}
+        
+    @property
+    def channels(self):
+        """
+        Return and cache channel list
+        """
+        if not self._channels:
+            req = requests.get('http://192.168.1.200:8081/channels/')
+            soup = BeautifulSoup(req.text, 'html.parser')
+            self._channels = {}
+            for channel in soup.find_all('channel'):
+                self._channels[channel['id']] = {
+                    'name': channel['name'],
+                    'group': channel['group'],
+                    'adult': channel['adult'],
+                    'epg_id': channel['epg_id'],
+                }
+        return self._channels
+                
 
 class DefaultConfig(object):
     """
@@ -12,6 +35,9 @@ class DefaultConfig(object):
     """
     IP = "127.0.0.1"
     PORT = "8081"
+
+# Initialise application
+app = RecordManagerServer(__name__)
 
 # Initialise configuration
 app.config.from_object('record_manager.DefaultConfig')
@@ -23,9 +49,12 @@ def index():
     Index page with the list of main links to display
     """
     # Check if p2p proxy server is accessible
-    req = requests.get('http://{}:{}/stat'.format(
-        app.config['IP'], app.config['PORT']))
-    ok = bool(req.status_code == 200)
+    try:
+        req = requests.get('http://{}:{}/stat'.format(
+            app.config['IP'], app.config['PORT']), timeout=2)
+        ok = bool(req.status_code == 200)
+    except:
+        ok = False
     return render_template('index.html', ok=ok)
 
 @app.route('/channels')
@@ -33,7 +62,9 @@ def channels():
     """
     Channels link
     """
-    return render_template('channels.html')
+    channels = app.channels
+    return render_template('channels.html', channels=channels)
+    
 
 @app.route('/records')
 def records():

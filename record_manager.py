c0370244a681cae48cff49b1c974889104418dca
Said Babayev <said@indeed.com>
2015-12-25 17:49:49 +0000

    Record add

--- record_manager.py
+++ record_manager.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
+import re
 import requests
 
+from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 from flask import Flask, Config
-from flask import render_template
+from flask import render_template, flash, redirect
+
+from forms import RecordForm
 
 class RecordManagerServer(Flask):
     def __init__(self, *args, **kwargs):
         super(RecordManagerServer, self).__init__(*args, **kwargs)
-        self._channels = {}
+        self._channels = []
+        self._records = []
         
     @property
     def channels(self):
@@ -16,17 +21,42 @@ class RecordManagerServer(Flask):
         Return and cache channel list
         """
         if not self._channels:
+            requests.get('http://192.168.1.200:8081/login')
             req = requests.get('http://192.168.1.200:8081/channels/')
             soup = BeautifulSoup(req.text, 'html.parser')
-            self._channels = {}
             for channel in soup.find_all('channel'):
-                self._channels[channel['id']] = {
-                    'name': channel['name'],
-                    'group': channel['group'],
-                    'adult': channel['adult'],
-                    'epg_id': channel['epg_id'],
-                }
+                self._channels.append(
+                    (
+                        channel['id'],
+                        channel['name'],
+                        channel['group'],
+                        channel['adult'],
+                        channel['epg_id'],
+                    )
+                )
+            self._channels = sorted(self._channels, key=lambda x: x[1])
         return self._channels
+    
+    @property
+    def records(self):
+        """
+        Return and cache records list
+        """
+        if not self._records:
+            req = requests.get('http://192.168.1.200:8081/records/')
+            soup = BeautifulSoup(req.text, 'html.parser')
+            channel_name = re.compile(r'(\[.*\])\s(.*)')
+            for record in soup.find_all('record'):
+                self._records.append(
+                    (
+                        channel_name.search(record['name']).group(2),
+                        datetime.strptime(record['start'], '%d%m%Y_%H%M%S'),
+                        datetime.strptime(record['end'], '%d%m%Y_%H%M%S'),
+                        record['status'],
+                    )
+                )
+            self._records = sorted(self._records, key=lambda x: x[0])
+        return self._records
                 
 
 class DefaultConfig(object):
@@ -35,6 +65,8 @@ class DefaultConfig(object):
     """
     IP = "127.0.0.1"
     PORT = "8081"
+    #WTF_CSRF_ENABLED = False
+    SECRET_KEY = "Testtesttest"
 
 # Initialise application
 app = RecordManagerServer(__name__)
@@ -71,7 +103,25 @@ def records():
     """
     Records link
     """
-    return render_template('records.html')
+    records = app.records
+    return render_template('records.html', records=records)
+
+@app.route('/records/add', methods=('GET', 'POST'))
+def records_form():
+    """
+    Form handling for records
+    """
+    form = RecordForm()
+    form.channel_id.choices = [ (channel[0], channel[1])
+        for channel in app.channels ]
+    now = datetime.now() + timedelta(hours=2)
+    later = now + timedelta(hours=2)
+    form.start.description = 'e.g. {}'.format(now.strftime('%d-%m-%Y %H:%M'))
+    form.end.description = 'e.g. {}'.format(later.strftime('%d-%m-%Y %H:%M'))
+    if form.validate_on_submit():
+        flash('test')
+        return redirect('/records')
+    return render_template('records_add.html', form=form)
 
 @app.route('/settings')
 def settings():

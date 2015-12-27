1f18d37b4550b66c8957ace5c6892905f1b47a85
Said Babayev <said@indeed.com>
2015-12-27 20:18:20 +0000

    Added setup.py

--- record_manager/record_manager.py
+++ record_manager/record_manager.py
@@ -181,4 +181,4 @@ def orig():
     return render_template('index_orig.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='p2pproxy-record-manager',
+    version='0.1',
+    description='Application to manage records for Local TS Proxy.',
+    long_description=long_description,
+    author='Said Babayev',
+    author_email='phoenix49@gmail.com',
+    license='GPLv3',
+    url='https://github.com/ph0enix49/p2pproxy-record-manager',
+    packages=['record_manager'],
+    install_requires=[
+        'beautifulsoup4>=4.4.0',
+        'Flask>=0.10.0',
+        'Flask-WTF',
+        'Jinja2',
+        'Werkzeug',
+        'WTForms>=2.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'record_manager=record_manager.record_manager:main',
+        ],
+    },
+)
